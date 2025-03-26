f45b8653c095bf926a36ebd2fa8c291937fc5365
giovanni.tizzoni <giovanni.tizzoni150104@gmail.com>
2025-03-26 18:03:36 +0100

    finish poker, need to add doc strings

--- deck.py
+++ deck.py
@@ -3,7 +3,7 @@ import random
 
 class Card:
     RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-    SUITS = ["Clubs", "Spades", "Hearts", "Diamonds"]
+    SUITS = ["♣️", "♠️", "♥️", "♦️"]
     def __init__(self, suit, rank):
         if rank not in self.RANKS:
             raise ValueError("Invalid Rank")
@@ -12,9 +12,14 @@ class Card:
         self._suit = suit
         self._rank = rank
 
+    def __eq__(self, other):
+        return self.rank == other.rank
+
+    def __gt__(self, other):
+        return self.RANKS.index(self.rank) > self.RANKS.index(other.rank)
 
     def __str__(self):
-        return f"{self._rank} of {self._suit}"
+        return f"{self._rank}{self._suit}"
 
     def __repr__(self):
         return self.__str__()
--- poker.py
+++ poker.py
@@ -22,14 +22,56 @@ class PokerHand:
                 return False
         return True
 
+    @property
+    def is_full_house(self):
+        return self.num_match == 8
+
+    @property
+    def num_match(self):
+        matches = 0
+        for i in range(len(self.cards)):
+            for j in range(len(self.cards)):
+                if i == j:
+                    continue
+                if self.cards[i].rank == self.cards[j].rank:
+                    matches += 1
+        return matches
+
+    @property
+    def is_pair(self):
+        if self.num_match == 2: # simpler
+            return True
+        return False
+
+    @property
+    def is_two_pair(self):
+        return self.num_match == 4 # advanced
+
+    @property
+    def is_tris(self):
+        return self.num_match == 6 # advanced
+
+    @property
+    def is_quad(self):
+        return self.num_match == 12 # advanced
+
+    @property
+    def is_straight(self):
+        self.cards.sort()
+        distance = Card.RANKS.index(self.cards[4].rank) - \
+                   Card.RANKS.index(self.cards[0].rank)
+        return self.num_match == 0 and distance == 4
+
 count = 0
-flushes = 0
-while flushes < 1000:
+matches = 0
+while matches < 1000:
     deck = Deck()
     deck.shuffle()
     hand = PokerHand(deck)
-    if hand.is_flush:
-        flushes += 1
+
+    if hand.is_straight:
+        matches += 1
+        print(hand)
     count += 1
 
-print(f"probability of a flush is {100*flushes/count}%")
+print(f"probability of a straight is {100*matches/count}%")
