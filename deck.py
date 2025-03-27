dfad590d5af326697f5eff104720fd0421de90ab
giovanni.tizzoni <giovanni.tizzoni150104@gmail.com>
2025-03-27 15:48:41 +0100

    finish poker, need to add doc strings

--- deck.py
+++ deck.py
@@ -3,9 +3,18 @@ from getopt import gnu_getopt
 
 
 class Card:
+    """
+    Deck cards with different type of suits and ranks
+    """
     RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     SUITS = ["♣️", "♠️", "♥️", "♦️"]
     def __init__(self, suit, rank):
+        """
+        Creates objects with suit and rank
+        :param suit: suit of the card
+        :param rank: rank of the card
+        Return 'ValueError' if suit or rank is not in the list 'RANKS' or 'SUITS'
+        """
         if rank not in self.RANKS:
             raise ValueError("Invalid Rank")
         if suit not in self.SUITS:
@@ -14,39 +23,80 @@ class Card:
         self._rank = rank
 
     def __eq__(self, other):
+        """
+        Compare cards to see if they are the same based on their rank
+        :param other: new card object
+        :return: True if the cards are the same, False if opposit
+        """
         return self.rank == other.rank
 
     def __gt__(self, other):
+        """
+        Compare cards to see which one has a higher rank
+        :param other: new card object
+        :return: True if card has higher rank, False if opposit
+        """
         return self.RANKS.index(self.rank) > self.RANKS.index(other.rank)
 
     def __str__(self):
+        """
+        String representation of the card
+        :return: string with [rank, suit]
+        """
         return f"{self._rank}{self._suit}"
 
     def __repr__(self):
+        """
+        Return the representation of the card
+        :return: string card
+        """
         return self.__str__()
 
     @property
     def suit(self):
+        """
+        Determine the suit of the card
+        :return: suit of the card
+        """
         return self._suit
 
     @property
     def rank(self):
+        """
+        Determine rank of the card
+        :return: rank of the card
+        """
         return self._rank
 
 class Deck:
     def __init__(self):
+        """
+        Creates deck of cards with 52 cards
+        """
         self._deck = []
         for suit in Card.SUITS:
             for rank in Card.RANKS:
                 self._deck.append(Card(suit,rank))
 
     def __str__(self):
+        """
+        String representation of the deck
+        :return: string with 52 cards
+        """
         return str(self._deck)
 
     def shuffle(self):
+        """
+        Shuffle the deck randomly
+        :return: shuffled deck
+        """
         random.shuffle(self._deck)
 
     def deal(self):
+        """
+        Remove and return the top card from the deck
+        :return: top card from the deck
+        """
         return self._deck.pop(0)
 
 if __name__ == "__main__":
--- poker.py
+++ poker.py
@@ -3,8 +3,15 @@ from getopt import gnu_getopt
 from deck import Deck, Card
 
 class PokerHand:
+    """
+    A class that represents a poker hand, therefore 5 cards drawn from the deck. Evaluates different poker hands and 'points'.
+    Starting from pair, two pairs, tris, straight, flush, full house, poker, color flush and royal flush
+    """
     def __init__(self, deck):
-
+        """
+        Creates a PokerHand by drawing 5 random cards from the previous given random deck of 52 cards
+        :param deck:
+        """
         cards = []
         for i in range(5):
             cards.append(deck.deal())
@@ -12,13 +19,26 @@ class PokerHand:
 
     @property
     def cards(self):
+        """
+        Get a list of 5 cards in the hand
+        :return: five cards
+        """
         return self._cards
 
     def __str__(self):
+        """
+        Return as output a string representation of the poker hand
+        :return: string containing 5 cards
+        """
         return str(self.cards)
 
     @property
     def is_flush(self):
+        """
+        Check if the hand is a flush, so if all cards have the same suit. It compares the suit of all cards with the first card's suit.
+        Compares the suit of the first card (self.cards[0].suit) with the suit of each of the other cards (self.cards[1:])
+        :return: True if they have same suit, False otherwise
+        """
         for card in self.cards[1:]:
             if self.cards[0].suit != card.suit:
                 return False
@@ -26,10 +46,20 @@ class PokerHand:
 
     @property
     def is_full_house(self):
+        """
+        Check if the hand is a full house, consists of a tris and a pair.
+        The num_match property returns the total number of rank matches in the hand. A full house has exactly 8 rank matches (three matches for one rank and two matches for another)
+        :return: True if full house, False if opposite
+        """
         return self.num_match == 8
 
     @property
     def num_match(self):
+        """
+        Counts the number of matching cards in the given hand drawn randomly. It is done by comparing cards with every other card in the hand
+        Determines various hands type like pairs, tris, and a full house
+        :return: total number of matches
+        """
         matches = 0
         for i in range(len(self.cards)):
             for j in range(len(self.cards)):
@@ -41,24 +71,47 @@ class PokerHand:
 
     @property
     def is_pair(self):
+        """
+        Checks if the hand has a single pair. 'num_match' property is used here to count the number of rank matches
+        :return: True if the hand has one pair, False if opposite
+        """
         if self.num_match == 2: # simpler
             return True
         return False
 
     @property
     def is_two_pair(self):
+        """
+        Check if the hand contains two pairs. It consists of two different ranks, each with two cards. If num_match = 4 then there are two pairs
+        :return: True if the hand has two pairs, otherwise False.
+        """
         return self.num_match == 4 # advanced
 
     @property
     def is_tris(self):
+        """
+        Check if the hand contains three cards of the same rank. If num_match = 6 then there is a tris
+        :return: True if the hand is a three-of-a-kind, otherwise False.
+        """
         return self.num_match == 6 # advanced
 
     @property
     def is_quad(self):
+        """
+        Check if the hand contains four cards of the same rank. If num_match = 12 then there is a quad
+        :return: True if the hand is a four-of-a-kind, otherwise False.
+        """
         return self.num_match == 12 # advanced
 
     @property
     def is_straight(self):
+        """
+        Check if the hand is a straight (five consecutive ranks).
+        The hand is sorted, and the difference between the highest and lowest rank is checked. The distance between the lowest and highest cards
+        in the sorted hand is calculated by comparing their positions in the Card.RANKS list.
+        If the distance between the highest and lowest ranks is 4, and there are no matching ranks (num_match == 0), the hand is a straight.
+        :return: True if the hand is a straight, otherwise False.
+        """
         self.cards.sort()
         distance = Card.RANKS.index(self.cards[4].rank) - \
                    Card.RANKS.index(self.cards[0].rank)
